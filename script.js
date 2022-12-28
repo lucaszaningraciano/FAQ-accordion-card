f841fb19db6f55321493960644ec5a0b02312bdd
LUCAS ZANIN GRACIANO <lucas.zanin.graciano@hotmail.com>
2022-12-28 17:05:35 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+function abrirmenu1() {
+  let PrimeiroMenu = document.getElementById("small-um");
+  if (PrimeiroMenu.style.display == "none") {
+    PrimeiroMenu.style.display = "block";
+  } else {
+    PrimeiroMenu.style.display = "none";
+  }
+}
+function abrirmenu2() {
+  let SegundoMenu = document.getElementById("small-dois");
+  if (SegundoMenu.style.display == "none") {
+    SegundoMenu.style.display = "block";
+  } else {
+    SegundoMenu.style.display = "none";
+  }
+}
+
+function abrirmenu3() {
+  let TerceiroMenu = document.getElementById("small-tres");
+  if (TerceiroMenu.style.display == "none") {
+    TerceiroMenu.style.display = "block";
+  } else {
+    TerceiroMenu.style.display = "none";
+  }
+}
+
+function abrirmenu4() {
+  let QuartoMenu = document.getElementById("small-quatro");
+  if (QuartoMenu.style.display == "none") {
+    QuartoMenu.style.display = "block";
+  } else {
+    QuartoMenu.style.display = "none";
+  }
+}
+function abrirmenu5() {
+  let QuintoMenu = document.getElementById("small-cinco");
+  if (QuintoMenu.style.display == "none") {
+    QuintoMenu.style.display = "block";
+  } else {
+    QuintoMenu.style.display = "none";
+  }
+}
